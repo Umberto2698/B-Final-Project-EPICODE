b271722e2618cb4107b3e0b103e061a478dd8732
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-12-02 22:49:25 +0100

    Added login and register endpoint

---
+++ src/main/java/finalproject/authorization/AuthController.java
@@ -0,0 +1,34 @@
+package finalproject.authorization;
+
+import finalproject.exceptions.BadRequestException;
+import finalproject.user.User;
+import finalproject.user.payloads.UserLoginDTO;
+import finalproject.user.payloads.UserRegisterDTO;
+import finalproject.user.payloads.UserSuccessLoginDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public UserSuccessLoginDTO login(@RequestBody UserLoginDTO body) throws Exception {
+        return new UserSuccessLoginDTO(authService.authenticateUser(body));
+    }
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User saveUser(@RequestBody @Validated UserRegisterDTO body, BindingResult validation) {
+        if (validation.hasErrors()) {
+            throw new BadRequestException("", validation.getAllErrors());
+        } else {
+            return authService.save(body);
+        }
+    }
+}
---
+++ src/main/java/finalproject/authorization/AuthService.java
@@ -0,0 +1,73 @@
+package finalproject.authorization;
+
+import finalproject.exceptions.BadRequestException;
+import finalproject.exceptions.UnauthorizedException;
+import finalproject.security.JWTTools;
+import finalproject.user.User;
+import finalproject.user.UserRepository;
+import finalproject.user.UserService;
+import finalproject.user.payloads.UserLoginDTO;
+import finalproject.user.payloads.UserRegisterDTO;
+import finalproject.user.payloads.UserUpdateInfoDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class AuthService {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private JWTTools jwtTools;
+    @Autowired
+    private PasswordEncoder bcrypt;
+
+    public String authenticateUser(UserLoginDTO body) {
+        User user = userService.findByEmail(body.email());
+        if (bcrypt.matches(body.password(), user.getPassword())) {
+            return jwtTools.createToken(user);
+        } else {
+            throw new UnauthorizedException("Email or password invalid.");
+        }
+    }
+
+    public User save(UserRegisterDTO body) {
+        userRepository.findByEmail(body.email()).ifPresent(a -> {
+            throw new BadRequestException("The email " + a.getEmail() + " is alredy used.");
+        });
+        User user = new User();
+        user.setEmail(body.email());
+        user.setName(body.name());
+        user.setSurname(body.surname());
+        user.setPassword(bcrypt.encode(body.password()));
+
+        return userRepository.save(user);
+    }
+
+    public User update(UUID id, UserUpdateInfoDTO body) {
+        User found = userService.getById(id);
+        if (!body.email().isEmpty()) {
+            found.setEmail(body.email());
+        }
+        if (!body.name().isEmpty()) {
+            found.setName(body.name());
+        }
+        if (!body.surname().isEmpty()) {
+            found.setSurname(body.surname());
+        }
+        if (!body.password().isEmpty()) {
+            found.setPassword(bcrypt.encode(body.password()));
+        }
+        if (!body.address().isEmpty()) {
+            found.setAddress(body.address());
+        }
+        if (body.birthday() != null) {
+            found.setBirthday(body.birthday());
+        }
+        return userRepository.save(found);
+    }
+}
