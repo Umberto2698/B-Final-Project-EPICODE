7b42e6b1fbfdff8e992e9fcb4e70141a1b507b05
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-12-18 19:06:36 +0100

    updated some line of code for front-end

--- src/main/java/finalproject/donation/payloads/DonationUpdateDTO.java
+++ src/main/java/finalproject/donation/payloads/DonationUpdateDTO.java
@@ -1,13 +1,12 @@
 package finalproject.donation.payloads;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import finalproject.donation.enums.Check;
 import finalproject.donation.validators.ValidCheck;
 
 import java.time.LocalDate;
 
 public record DonationUpdateDTO(
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+
         LocalDate donationDate,
         @ValidCheck(enumClass = Check.class,
                 message = "Check state not valid")
--- src/main/java/finalproject/donation/payloads/NewDonationDTO.java
+++ src/main/java/finalproject/donation/payloads/NewDonationDTO.java
@@ -1,12 +1,11 @@
 package finalproject.donation.payloads;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 public record NewDonationDTO(
         @NotNull(message = "donation date is required.")
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+
         LocalDate donationDate) {
 }
